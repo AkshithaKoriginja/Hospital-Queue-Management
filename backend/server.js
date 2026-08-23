f23cfa6046d8f115061e67552db4adb97ef120aa
AkshithaKoriginja <naveen.koriginja@gmail.com>
2026-08-23 20:27:39 +0530

    Implement test route for MySQL connection
    Added a test route to check MySQL database connection.

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,34 @@
 const express = require("express");
 const cors = require("cors");
 
+const db = require("./db");
+
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Test route
+// Test backend
 app.get("/", (req, res) => {
     res.send("Hospital Queue Management Backend is running!");
 });
 
-// Start server
+// Test MySQL connection
+app.get("/test-db", (req, res) => {
+    db.query("SELECT 1", (err, result) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({
+                message: "Database connection failed"
+            });
+        }
+
+        res.json({
+            message: "MySQL database connected successfully!"
+        });
+    });
+});
+
 const PORT = 5000;
 
 app.listen(PORT, () => {
