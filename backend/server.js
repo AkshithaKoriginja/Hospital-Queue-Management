7dd416d507598cd73cb50391e142700f6141fc37
AkshithaKoriginja <naveen.koriginja@gmail.com>
2026-08-23 22:32:43 +0530

    Integrate patient routes and enhance DB test endpoint
    Added patient routes and improved database test response.

--- backend/server.js
+++ backend/server.js
@@ -2,22 +2,26 @@ const express = require("express");
 const cors = require("cors");
 
 const db = require("./db");
+const patientRoutes = require("./routes/patients");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Test backend
+
+// Home route
 app.get("/", (req, res) => {
     res.send("Hospital Queue Management Backend is running!");
 });
 
-// Test MySQL connection
+
+// Test MySQL
 app.get("/test-db", (req, res) => {
+
     db.query("SELECT 1", (err, result) => {
+
         if (err) {
-            console.error(err);
             return res.status(500).json({
                 message: "Database connection failed"
             });
@@ -27,8 +31,15 @@ app.get("/test-db", (req, res) => {
             message: "MySQL database connected successfully!"
         });
     });
+
 });
 
+
+// Patient API
+app.use("/patients", patientRoutes);
+
+
+// Start server
 const PORT = 5000;
 
 app.listen(PORT, () => {
